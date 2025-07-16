505848950900893e425199bf2e77e8be53e59f1b
Angel Blanco <utti222027@utvco.edu.mx>
2025-07-16 09:33:46 -0600

    clonado y configuracion Backend2

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,16 +7,16 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'tiendita_db',
-      autoLoadEntities:true,
+      port: 5432,
+      username: 'postgres',
+      password: 'blanco03',
+      database: 'tienda_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
-    UsuariosModule
+    UsuariosModule,
   ],
   controllers: [AppController],
   providers: [AppService],

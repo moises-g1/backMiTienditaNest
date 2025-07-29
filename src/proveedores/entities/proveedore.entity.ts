9dfffc8df8cf741617d2426d828c4838b5e99f52
Angel Blanco <utti222027@utvco.edu.mx>
2025-07-29 08:21:10 -0600

    proveedores CRUD creado

--- src/proveedores/entities/proveedore.entity.ts
+++ src/proveedores/entities/proveedore.entity.ts
@@ -1,1 +1,39 @@
-export class Proveedore {}
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('proveedores')
+export class Proveedor {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  nombre: string;
+
+  @Column()
+  apellido: string;
+
+  @Column()
+  edad: number;
+
+  @Column()
+  tipo_producto: string;
+
+  @Column()
+  telefono: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  direccion: string;
+
+  @Column({ default: true })
+  activo: boolean;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  created_at: Date;
+}

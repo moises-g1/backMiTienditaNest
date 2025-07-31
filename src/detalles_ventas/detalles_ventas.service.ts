bf7bb961cfda95cdf730789328ff8d4db9976b1e
Angel Blanco <utti222027@utvco.edu.mx>
2025-07-31 12:08:11 -0600

    Crud creado (sin pruebas en postman)

--- src/detalles_ventas/detalles_ventas.service.ts
+++ src/detalles_ventas/detalles_ventas.service.ts
@@ -1,26 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { DetallesVenta } from './entities/detalles_venta.entity';
 import { CreateDetallesVentaDto } from './dto/create-detalles_venta.dto';
-import { UpdateDetallesVentaDto } from './dto/update-detalles_venta.dto';
+import { Venta } from '../ventas/entities/venta.entity';
+import { Producto } from '../productos/entities/producto.entity';
 
 @Injectable()
 export class DetallesVentasService {
-  create(createDetallesVentaDto: CreateDetallesVentaDto) {
-    return 'This action adds a new detallesVenta';
+  constructor(
+    @InjectRepository(DetallesVenta)
+    private readonly detalleRepo: Repository<DetallesVenta>,
+
+    @InjectRepository(Producto)
+    private readonly productoRepo: Repository<Producto>,
+
+    @InjectRepository(Venta)
+    private readonly ventaRepo: Repository<Venta>,
+  ) {}
+  async create(dto: CreateDetallesVentaDto) {
+    const producto = await this.productoRepo.findOneBy({ id: dto.productoId });
+    if (!producto) {
+      throw new HttpException('Producto no encontrado', HttpStatus.NOT_FOUND);
+    }
+
+    if (producto.stock_actual < dto.cantidad) {
+      throw new HttpException(
+        'Stock insuficiente para realizar la venta',
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    // Restar stock
+    producto.stock_actual -= dto.cantidad;
+    await this.productoRepo.save(producto);
+
+    const detalle = this.detalleRepo.create({
+      cantidad: dto.cantidad,
+      precio_unitario: dto.precio_unitario,
+      producto: { id: dto.productoId },
+      venta: { id: dto.ventaId },
+    });
+
+    return this.detalleRepo.save(detalle);
   }
 
   findAll() {
-    return `This action returns all detallesVentas`;
+    return this.detalleRepo.find({ relations: ['venta', 'producto'] });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} detallesVenta`;
-  }
-
-  update(id: number, updateDetallesVentaDto: UpdateDetallesVentaDto) {
-    return `This action updates a #${id} detallesVenta`;
+    return this.detalleRepo.findOne({
+      where: { id },
+      relations: ['venta', 'producto'],
+    });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} detallesVenta`;
+    return this.detalleRepo.delete(id);
   }
 }
